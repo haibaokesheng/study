9fdd1c890fab681c39808b8aec03e05f28cb9d93
haibaokesheng <767208457@qq.com>
2020-05-05 20:10:15 +0800

    '5月5日'

--- BFS.py
+++ BFS.py
@@ -6,44 +6,52 @@
 @Description: 图的BFS和DFS
 @FilePath: \刷题人生\BFS.py
 '''
-graph ={
-    "A":["B","C"],
-    "B":['A','C','D'],
-    'C':['A','B','D','E'],
-    'D':['B','C','E','F'],
-    'E':['C','D'],
-    'F':['D']
-    }
+graph = {
+    "A": ["B", "C"],
+    "B": ['A', 'C', 'D'],
+    'C': ['A', 'B', 'D', 'E'],
+    'D': ['B', 'C', 'E', 'F'],
+    'E': ['C', 'D'],
+    'F': ['D']
+}
 print(graph.keys())
 
-def BFS(graph,s):#最短路径
+
+def BFS(graph, s):  # 最短路径
     quene = []
     quene.append(s)
     seen = set()
     seen.add(s)
-    parent = {s:None}
+    parent = {s: None}
 
-    while(len(quene)>0):
-        vertex = quene.pop(0)
+    while quene:
+        vertex = quene.pop(0)  # vertex顶点
         nodes = graph[vertex]
         for w in nodes:
             if w not in seen:
                 quene.append(w)
                 seen.add(w)
-                parent[w]=vertex
-        print(vertex)
+                parent[w] = vertex
+        print(vertex, '->', end=' ')
     return parent
-print('图的BFS')
-parent = BFS(graph,'A')
-for key in parent:
-    print(key,parent[key])
 
-def DFS(graph,s):
+
+print('图的BFS:', end=' ')
+parent = BFS(graph, 'A')
+print('\n')
+for key in parent:
+    print(key,'->',end=' ')
+print('\n')
+for key in parent:
+    print(parent[key],'',end='')
+
+
+def DFS(graph, s):
     stack = []
     stack.append(s)
     seen = set()
     seen.add(s)
-    while(len(stack)>0):
+    while(len(stack) > 0):
         vertex = stack.pop(0)
         nodes = graph[vertex]
         for w in nodes:
@@ -51,5 +59,7 @@ def DFS(graph,s):
                 stack.append(w)
                 seen.add(w)
         print(vertex)
+
+
 print('图的DFS')
-DFS(graph,'A')       
+DFS(graph,'A')
